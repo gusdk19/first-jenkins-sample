8a52a5705b8f425e4325a879ce366075675afb1a
gusdk19 <hyuna030619@naver.com>
2024-10-08 16:57:50 +0900

    webhook 작동 확인

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "Nihaoo World!";
+        return "Nihaoowwwwwww World!";
     }
 }
